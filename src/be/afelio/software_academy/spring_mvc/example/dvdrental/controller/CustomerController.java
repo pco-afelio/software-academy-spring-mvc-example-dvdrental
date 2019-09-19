ad6b0234ce28c3e0d559728737623b96fce9508e
pco <pco@afelio.be>
2019-09-19 12:25:01 +0200

    begin put method

--- src/be/afelio/software_academy/spring_mvc/example/dvdrental/controller/CustomerController.java
+++ src/be/afelio/software_academy/spring_mvc/example/dvdrental/controller/CustomerController.java
@@ -1,16 +1,20 @@
 package be.afelio.software_academy.spring_mvc.example.dvdrental.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import be.afelio.software_academy.spring_mvc.example.dvdrental.dto.CustomerDto;
 import be.afelio.software_academy.spring_mvc.example.dvdrental.dto.ResponseDto;
 import be.afelio.software_academy.spring_mvc.example.dvdrental.dto.ResponseDtoStatus;
+import be.afelio.software_academy.spring_mvc.example.dvdrental.dto.UpdateCustomerEmailDto;
 import be.afelio.software_academy.spring_mvc.example.dvdrental.persistence.ApplicationRepository;
 
 @Controller
@@ -41,6 +45,14 @@ public class CustomerController {
 		
 		return ResponseEntity.ok(dto);
 	}
+	
+	@PutMapping(produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseDto> updateCustomerEmail(@RequestBody UpdateCustomerEmailDto updateCustomerEmailDto) {
+		ResponseDto dto = new ResponseDto(ResponseDtoStatus.SUCCESS, "ok");
+		System.out.println("CustomerController.updateCustomerEmail() => " + updateCustomerEmailDto);
+		return ResponseEntity.ok(dto);
+	}
+	
 }
 
 
